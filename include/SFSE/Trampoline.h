15d667d4472d733306c6b075c48430776cc5c8f9
Qudix <17361645+Qudix@users.noreply.github.com>
2026-01-23 19:10:50 -0600

    chore: deprecation

--- include/SFSE/Trampoline.h
+++ include/SFSE/Trampoline.h
@@ -4,10 +4,10 @@
 
 namespace SFSE
 {
-	using Trampoline = REL::Trampoline;
+	using Trampoline [[deprecated("Use REL::Trampoline instead")]] = REL::Trampoline;
 
 	// DEPRECATED
-	[[nodiscard]] inline REL::Trampoline& GetTrampoline() noexcept
+	[[nodiscard, deprecated("Use REL::GetTrampoline instead")]] inline REL::Trampoline& GetTrampoline() noexcept
 	{
 		return REL::GetTrampoline();
 	}
